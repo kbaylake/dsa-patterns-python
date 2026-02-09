4f7ea1944c6acfe7c50be2a000938fd7db3cd865
Karan Bedi <kbedi03@gmail.com>
2026-02-09 19:46:11 +0530

    Found a better way to solve clone graph

--- DailyLeetCode/CloneGraphFin.py
+++ DailyLeetCode/CloneGraphFin.py
@@ -1,5 +1,12 @@
 # Date: 08-02-2026
 # #133. Clone Graph
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 from collections import deque
 from typing import Optional
 class Solution:
@@ -11,22 +18,25 @@ class Solution:
             queue.append(node)
             visited={}
             while queue:
-                for i in range(len(queue)):
-                    curr=queue[0]
-                    queue.popleft()
-                    neigh=[]
-                    val=curr.val
-                    newNode=Node()
-                    newNode.val=val
+                curr=queue.popleft()
+                if curr not in visited:
+                    newNode=Node(curr.val)
                     visited[curr]=newNode
-                    for v in curr.neighbors:
-                        if v in visited:
-                            vNeigh=visited[v].neighbors
-                            vNeigh.append(newNode)
-                            visited[v].neighbors=vNeigh
-                            neigh.append(v)
-                        else:
-                            queue.append(v)
-                    newNode.neighbors=neigh
+                    neigh=[]
+                else:
+                    newNode=visited[curr]
+                    neigh=newNode.neighbors
+                for v in curr.neighbors:
+                    if v not in visited:
+                        newNeigh=Node(v.val,[newNode])
+                        neigh.append(newNeigh)
+                        visited[v]=newNeigh
+                        queue.append(v)
+                    else:
+                        newNeighs=visited[v]
+                        if newNode not in newNeighs.neighbors:
+                            newNeighs.neighbors.append(newNode)
+                            neigh.append(newNeighs)
+                newNode.neighbors=neigh
             return visited[node]
         return bfs(node)
