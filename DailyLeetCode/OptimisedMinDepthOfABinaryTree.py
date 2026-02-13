1692025ddbbf72dce1bce0cbc192ec06ef8f0462
Karan Bedi <kbedi03@gmail.com>
2026-02-13 22:29:54 +0530

    Optimised ax diff between a node in a binary tree

--- DailyLeetCode/OptimisedMinDepthOfABinaryTree.py
+++ DailyLeetCode/OptimisedMinDepthOfABinaryTree.py
@@ -7,16 +7,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math
-from typing import Optional
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int: # type: ignore
-        def getMax(root,mini,maxi):
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        def getDepth(root):
             if not root:
-                return maxi-mini
-            mini=min(mini,root.val)
-            maxi=max(maxi,root.val)
-            left=getMax(root.left,mini,maxi)
-            right=getMax(root.right,mini,maxi)
-            return max(left,right)   
-        return getMax(root,root.val,root.val)
+                return 0
+            if not root.left:
+                return getDepth(root.right)+1
+            elif not root.right:
+                return getDepth(root.left)+1
+            return(1 + min(getDepth(root.left), getDepth(root.right)))
+        return getDepth(root)
