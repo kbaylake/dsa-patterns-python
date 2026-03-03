365dd2021a029e1b2667d18855107cdd692ac784
Karan Bedi <kbedi03@gmail.com>
2026-03-03 19:48:06 +0530

    Solved a non linear sliding window problem

---
+++ DailyLeetCode/MaximumPointsYouCanObtainFromCards.py
@@ -0,0 +1,15 @@
+#03-03-2026
+#1423. Maximum Points you can obtain from cards
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        right=-1
+        if k==len(cardPoints):
+            return sum(cardPoints)
+        left=k-1
+        ans=curr=sum(cardPoints[0:k])
+        while left>=0:
+            curr=curr-cardPoints[left]+cardPoints[right]
+            ans=max(ans,curr)
+            left-=1
+            right-=1
+        return ans
