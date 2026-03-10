df70b45cfcda8091a7e51224c943f91f8c310417
Karan Bedi <kbedi03@gmail.com>
2026-03-10 20:13:56 +0530

    Revising stacks

---
+++ DailyLeetCode/SimplifyPath.py
@@ -0,0 +1,21 @@
+#10-03-2026
+#71. Simplify Path
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        pathl = [p for p in path.split('/') if p]
+        print(pathl)
+        for i in pathl:
+            if stack:
+                if i=='.':
+                    continue
+                if i=='..':
+                    stack.pop()
+                    continue
+            else:
+                if i=='.' or i=='..':
+                    stack.append('/')
+                    continue
+            stack.append(f'/{i}')
+        return ''.join(stack)
+        
