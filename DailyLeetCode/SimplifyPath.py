a3e870d437c88280bdfae8c630d4fd99981392fc
Karan Bedi <kbedi03@gmail.com>
2026-03-10 20:25:37 +0530

    Found a smarter way of solving simplyify path

--- DailyLeetCode/SimplifyPath.py
+++ DailyLeetCode/SimplifyPath.py
@@ -4,10 +4,6 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack=[]
         pathl = [p for p in path.split('/') if p]
-        if not pathl:
-            return '/'
-        if len(pathl)==1 and (pathl[0]=='.' or pathl[0]=='..'):
-            return '/'
         for i in pathl:
             if stack:
                 if i=='.':
@@ -19,7 +15,4 @@ class Solution:
                 if i=='.' or i=='..':
                     continue
             stack.append(f'/{i}')
-        if not stack:
-            return '/'
-        return ''.join(stack)
-        
+        return ''.join(stack) if stack else '/'
